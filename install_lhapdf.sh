2d8be50c21ada23b216429fb409d9c8cb8303239
walten <wolfgang.waltenberger@gmail.com>
2021-07-23 11:20:16 +0200

    no bin

--- install_lhapdf.sh
+++ install_lhapdf.sh
@@ -4,6 +4,6 @@ wget https://lhapdf.hepforge.org/downloads/?f=LHAPDF-6.3.0.tar.gz -O LHAPDF-6.3.
 # ^ or use a web browser to download, which will get the filename correct
 tar xf LHAPDF-6.3.0.tar.gz
 cd LHAPDF-6.3.0
-./configure --prefix=~/.local/bin/
+./configure --prefix=~/.local/
 make
 make install
