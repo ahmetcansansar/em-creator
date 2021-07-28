085201a0392a4e4604f440e0e9d890140fbe978b
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2021-07-28 11:52:00 +0200

    recursive touch

--- touch_all.py
+++ touch_all.py
@@ -1,15 +1,27 @@
 #!/usr/bin/env python3
 
-import glob, subprocess
+import glob, subprocess, os
+
+def touch ( Dir, level=0 ):
+    """ touch file or directory. if Directory recursively touch
+        everything beneath """
+    cmd = f"touch {Dir}"
+    subprocess.getoutput ( cmd )
+    nfiles = 1
+    if os.path.isdir ( Dir ):
+        files = glob.glob ( Dir+"/*" )
+        for f in files:
+            nfiles += touch ( f, level+1 )
+    if level<2:
+        print ( f"{cmd}: {nfiles}" )
+    return nfiles
+
 
 def main():
-    files = glob.glob("mg5results/*.hepmc.gz" )
-    files += glob.glob("results/*.saf" )
-    files += glob.glob("results/*.dat" )
-    for f in files:
-        cmd = "touch %s" % f
-        print ( f )
-        o = subprocess.getoutput ( cmd )
-        print ( o )
+    touch ( "CutLang/" )
+    touch ( "delphes/" )
+    touch ( "embaked/" )
+    touch ( "mg5/" )
+#    touch ( "cutlang_results/" )
 
 main()
