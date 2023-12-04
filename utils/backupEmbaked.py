8c90b4b93c41cd7b1db5b714d61461a9d94410e8
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2023-12-04 10:43:48 +0100

    backups

--- utils/backupEmbaked.py
+++ utils/backupEmbaked.py
@@ -5,6 +5,9 @@ import glob, shutil, os, subprocess
 def backup():
     backup = "/groups/hephy/pheno/ww/embaked.backup/"
     backup2 = "/groups/hephy/pheno/ww/embaked.backup2/"
+    if not os.path.exists ( backup ):
+        backup=f"{os.environ['HOME']}/backup/"
+        backup2=f"{os.environ['HOME']}/backup2/"
     if not os.path.exists ( backup2 ):
         os.mkdir ( backup2 )
     if not os.path.exists ( backup ):
